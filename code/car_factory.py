c21478d08c0305c53383f4935b3192f4e8739245
Deniss Komarskis <deniss.komarskis@gmail.com>
2023-09-12 14:14:50 +0300

    refactor: refactored car factory

--- code/car_factory.py
+++ code/car_factory.py
@@ -11,42 +11,42 @@ from datetime import datetime
 
 class CarFactory():
     @staticmethod
-    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_calliope(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear):
         return Car(
             CapuletEngine(current_mileage, last_service_mileage),
             SpindlerBattery(current_date, last_service_date),
-            CarriganTires([0,0,0,0]))
+            CarriganTires(tire_wear))
 
     @staticmethod
-    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_glissade(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear):
         return Car(
             WilloughbyEngine(current_mileage, last_service_mileage),
             SpindlerBattery(current_date, last_service_date),
-            CarriganTires([0,0,0,0]))
+            OctoprimeTires(tire_wear))
 
     @staticmethod
-    def create_palindrome(current_date, last_service_date, warning_light_on):
+    def create_palindrome(current_date, last_service_date, warning_light_on, tire_wear):
         return Car(
             SternmanEngine(warning_light_on),
             SpindlerBattery(current_date, last_service_date),
             CarriganTires([0,0,0,0]))
 
     @staticmethod
-    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_rorschach(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear):
         return Car(
             WilloughbyEngine(current_mileage, last_service_mileage),
             NubbinBattery(current_date, last_service_date),
-            CarriganTires([0,0,0,0]))
+            OctoprimeTires(tire_wear))
 
     @staticmethod
-    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_thovex(current_date, last_service_date, current_mileage, last_service_mileage, tire_wear):
         return Car(
             CapuletEngine(current_mileage, last_service_mileage),
             NubbinBattery(current_date, last_service_date),
-            OctoprimeTires([0,0,0,0]))
+            CarriganTires(tire_wear))
     
-print(CarFactory.create_calliope(datetime.today().date(), datetime.today().date(), 100, 0))
-print(CarFactory.create_glissade(datetime.today().date(), datetime.today().date(), 100, 0))
-print(CarFactory.create_palindrome(datetime.today().date(), datetime.today().date(), False))
-print(CarFactory.create_rorschach(datetime.today().date(), datetime.today().date(), 100, 0))
-print(CarFactory.create_thovex(datetime.today().date(), datetime.today().date(), 100, 0))
+print(CarFactory.create_calliope(datetime.today().date(), datetime.today().date(), 100, 0, [0, 0, 0, 0]))
+print(CarFactory.create_glissade(datetime.today().date(), datetime.today().date(), 100, 0, [0, 0, 0, 0]))
+print(CarFactory.create_palindrome(datetime.today().date(), datetime.today().date(), False, [0, 0, 0, 0]))
+print(CarFactory.create_rorschach(datetime.today().date(), datetime.today().date(), 100, 0, [0, 0, 0, 0]))
+print(CarFactory.create_thovex(datetime.today().date(), datetime.today().date(), 100, 0, [0, 0, 0, 0]))
